f27f32fb42acb37522e9fdeb9f723d8d7ceedddf
mrarthurwhite <mister.arthur.white@gmail.com>
2020-02-26 14:49:19 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,7 +5,18 @@ class StudentsController < ApplicationController
     @students = Student.all
   end
 
+  def activate
+    id = params[:id]
+    @student = Student.find(id)
+    toggle_flag=!@student.active
+    @student.active = toggle_flag
+    @student.save
+    redirect_to  action:"show", id:id
+  end
+
   def show
+    #@student = Student.find(params[:id])
+    set_student
   end
 
   private
